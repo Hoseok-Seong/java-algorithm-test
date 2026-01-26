1c6cabd83635f9d2f8152b8b7d54433b5412441f
hoseokseong <hoselabs@gmail.com>
2026-01-26 23:52:23 +0900

    5. 특정 문자 뒤집기

---
+++ solution/q5/Main.java
@@ -0,0 +1,41 @@
+package q5;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public String solution(String word) {
+        char[] arr = word.toCharArray();
+
+        int lt = 0;
+        int rt = arr.length - 1;
+
+        while (lt < rt) {
+            if (!Character.isAlphabetic(arr[lt])) {
+                lt++;
+            } else if (!Character.isAlphabetic(arr[rt])) {
+                rt--;
+            }
+            else {
+                char tmp = arr[lt];
+                arr[lt] = arr[rt];
+                arr[rt] = tmp;
+
+                lt++;
+                rt--;
+            }
+        }
+
+        return String.valueOf(arr);
+    }
+    
+    public static void main(String[] args) throws Exception {
+        Main m = new Main();
+
+        System.out.println(m.solution(br.readLine()));
+    }
+}
