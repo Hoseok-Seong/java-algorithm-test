86db87b31b41b2377e391da55ba99dcdfdb8a424
hoseokseong <hoselabs@gmail.com>
2026-01-27 22:53:49 +0900

    6. 중복문자제거

---
+++ solution/q6/Main.java
@@ -0,0 +1,30 @@
+package q6;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public String solution(String word) {
+        String answer = "";
+
+        for (int i=0; i < word.length(); i++) {
+            if (word.indexOf(word.charAt(i)) == i) {
+                answer += word.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Main m = new Main();
+
+        String word = br.readLine();
+
+        System.out.println(m.solution(word));
+    }
+}
