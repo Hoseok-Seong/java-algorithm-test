ab9f15942dcf47af1484dc8d91267f26cc7079ef
hoseokseong <hoselabs@gmail.com>
2026-02-10 22:11:41 +0900

    3. 가위바위보

--- solution/section3/q3/Main.java
+++ solution/section3/q3/Main.java
@@ -8,17 +8,53 @@ public class Main {
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
 
-    public int solution(String word, char c) {
-        int answer = 0;
-        return answer;
+    public String[] solution(int no, String a, String b) {
+        int[] arrA = new int[no];
+        int[] arrB = new int[no];
+        String[] arrAnswer = new String[no];
+
+        st = new StringTokenizer(a);
+
+        for (int i=0; i<no; i++) {
+            arrA[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(b);
+
+        for (int i=0; i<no; i++) {
+            arrB[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 1 가위 2 바위 3 보
+        for (int i=0; i<no; i++) {
+            // 비기는 경우
+            if (arrA[i] == arrB[i]) {
+                arrAnswer[i] = "D";
+            }
+            // A가 이기는 경우
+            else if ((arrA[i] == 1 && arrB[i] == 3) || (arrA[i] == 2 && arrB[i] == 1) || (arrA[i] == 3 && arrB[i] == 2)) {
+                arrAnswer[i] = "A";   
+            }
+            // B가 이기는 경우
+            else {
+                arrAnswer[i] = "B";
+            }
+        }
+
+        return arrAnswer;
     }
 
     public static void main(String[] args) throws Exception {
         Main m = new Main();
 
-        String word = br.readLine();
-        char c = br.readLine().charAt(0);
+        int no = Integer.parseInt(br.readLine());
+        String a = br.readLine();
+        String b = br.readLine();
 
-        System.out.println(m.solution(word, c));
+        String[] arr = m.solution(no, a, b);
+
+        for (int i=0; i<no; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
