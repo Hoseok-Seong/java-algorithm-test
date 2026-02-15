457ce6c52aa36a980402fb355c60b43a92d16428
hoseokseong <hoselabs@gmail.com>
2026-02-15 22:50:02 +0900

    11. 임시반장 정하기(나중에 다시 풀어보기)

--- solution/section3/q11/Main.java
+++ solution/section3/q11/Main.java
@@ -4,9 +4,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public int solution(int n, String nos) {
-        int answer = 0;
-        return answer;
+    public int solution(int n, int[][] arr) {
+        int max = Integer.MIN_VALUE;
+        int leader = 0;
+
+        // 학생수만큼 for문
+        for (int i=0; i<n; i++) {
+            int cnt = 0;
+
+            for (int j=0; j<n; j++) {
+                if (i==j) continue;
+                // 1~5학년만큼 for문
+                for (int x=0; x<5; x++) {
+                    if (arr[i][x] == arr[j][x]) {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+
+            if (cnt > max) {
+                max = Math.max(max, cnt);
+                leader = i;
+            }
+        }
+        return leader+1;
     }
 
     public static void main(String[] args) throws Exception {
@@ -15,8 +37,16 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        String nos = br.readLine();
 
-        System.out.println(m.solution(n, nos));
+        int[][] arr = new int[n][5];
+
+        for (int i=0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j=0; j<5; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        System.out.println(m.solution(n, arr));
     }
 }
