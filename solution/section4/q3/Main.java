70aaf8363320cc3a513a78d96f71b9f997cef42d
hoseokseong <hoselabs@gmail.com>
2026-02-21 22:34:47 +0900

    3. 최대 매출

--- solution/section4/q3/Main.java
+++ solution/section4/q3/Main.java
@@ -4,9 +4,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public int solution(String word, char c) {
+    // 10 3
+    // 12 15 11 20 25 10 20 19 13 15
+    public int solution(int no, int days, String memo) {
+        int max = Integer.MIN_VALUE;
         int answer = 0;
-        return answer;
+
+        int[] arr = new int[no];
+
+        StringTokenizer st = new StringTokenizer(memo);
+
+        for (int i=0; i<no; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 초기 세팅
+        for (int i=0; i<days; i++) {
+            answer += arr[i];
+        }
+
+        max = answer;
+
+        for (int i=days; i<no; i++) {
+            answer -= arr[i-days];
+            answer += arr[i];
+
+            max = Math.max(max, answer);
+        }
+
+        return max;
     }
 
     public static void main(String[] args) throws Exception {
@@ -14,9 +40,12 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String word = br.readLine();
-        char c = br.readLine().charAt(0);
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        System.out.println(m.solution(word, c));
+        int no = Integer.parseInt(st.nextToken());
+        int days = Integer.parseInt(st.nextToken());
+        String memo = br.readLine();
+
+        System.out.println(m.solution(no, days, memo));
     }
 }
