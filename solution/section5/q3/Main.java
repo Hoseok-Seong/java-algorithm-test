3a9b6c127a28433ccd264f417136a2226c3b3e48
hoseokseong <hoselabs@gmail.com>
2026-02-22 16:05:20 +0900

    3. 매출액의 종류

--- solution/section5/q3/Main.java
+++ solution/section5/q3/Main.java
@@ -4,9 +4,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public int solution(String word, char c) {
-        int answer = 0;
-        return answer;
+    // 7 4
+    // 20 12 20 10 23 17 10
+    public String solution(int days, int seq, int[] arr) {
+        StringBuilder sb = new StringBuilder();
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i=0; i<seq-1; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        int lt=0;
+
+        for (int i=seq-1; i<days; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+
+            sb.append(map.size()).append(" ");
+
+            map.put(arr[lt], map.get(arr[lt]) - 1);
+            
+            if (map.get(arr[lt]) == 0) {
+                map.remove(arr[lt]);
+            }
+
+            lt++;
+        }
+
+        return sb.toString();
     }
 
     public static void main(String[] args) throws Exception {
@@ -14,9 +39,19 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String word = br.readLine();
-        char c = br.readLine().charAt(0);
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        System.out.println(m.solution(word, c));
+        int days = Integer.parseInt(st.nextToken());
+        int seq = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[days];
+
+        for (int i=0; i<days; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(m.solution(days, seq, arr));
     }
 }
