88043f9a9fe03bc78a262f9673ae562f8c27bd39
hoseokseong <hoselabs@gmail.com>
2026-02-22 16:21:55 +0900

    4. 모든 아나그램 찾기

--- solution/section5/q4/Main.java
+++ solution/section5/q4/Main.java
@@ -4,8 +4,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public int solution(String word, char c) {
+    // bacaAacba
+    // abc
+    public int solution(String word, String target) {
         int answer = 0;
+        int lt = 0;
+
+        HashMap<Character, Integer> targetMap = new HashMap<>();
+        HashMap<Character, Integer> wordMap = new HashMap<>();
+
+        // 타겟 세팅
+        for (char c : target.toCharArray()) {
+            targetMap.put(c, targetMap.getOrDefault(c, 0) + 1);
+        }
+
+        char[] arr = word.toCharArray();
+
+        for (int i=0; i<target.length() - 1; i++) {
+            wordMap.put(arr[i], wordMap.getOrDefault(arr[i], 0) + 1);
+        }
+
+        for (int i=target.length() - 1; i<word.length(); i++) {
+            wordMap.put(arr[i], wordMap.getOrDefault(arr[i], 0) + 1);
+            if (targetMap.equals(wordMap)) {
+                answer++;
+            }
+            wordMap.put(arr[lt], wordMap.getOrDefault(arr[lt], 0) - 1);
+
+            if (wordMap.get(arr[lt]) == 0) {
+                wordMap.remove(arr[lt]);
+            }
+
+            lt++;
+        }
+
         return answer;
     }
 
@@ -15,8 +47,8 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String word = br.readLine();
-        char c = br.readLine().charAt(0);
+        String target = br.readLine();
 
-        System.out.println(m.solution(word, c));
+        System.out.println(m.solution(word, target));
     }
 }
