43e9fc46ebea2fbf58183a6de0a26de8cec0cc4b
hoseokseong <hoselabs@gmail.com>
2026-02-28 22:52:50 +0900

    8. 응급실(다시 풀어보기)

--- solution/section6/q8/Main.java
+++ solution/section6/q8/Main.java
@@ -3,9 +3,55 @@ package section6.q8;
 import java.io.*;
 import java.util.*;
 
+class Person {
+    int id;
+    int priority;
+
+    public Person(int id, int priority) {
+        this.id = id;
+        this.priority = priority;
+    }
+}
+
 public class Main {
-    public int solution(String word, char c) {
+    // 5 2
+    // 60 50 70 80 90
+    public int solution(int person, int target, String danger) {
         int answer = 0;
+
+        StringTokenizer st = new StringTokenizer(danger);
+
+        Queue<Person> q = new ArrayDeque<>();
+
+        for (int i=0; i<person; i++) {
+            q.offer(new Person(i, Integer.parseInt(st.nextToken())));
+        }
+
+        Person tmp;
+
+        boolean bool = true;
+
+        while (bool) {
+            tmp = q.poll();
+            for (Person x : q) {
+                if (tmp.priority < x.priority) {
+                    q.offer(tmp);
+                    tmp = null;
+                    break;
+                } else {
+                    continue;
+                }
+            }
+
+            if (tmp != null) {
+                answer++;
+
+                if (tmp.id == target) {
+                    bool = false;
+                }
+            }
+        }
+
         return answer;
     }
 
@@ -14,9 +60,13 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String word = br.readLine();
-        char c = br.readLine().charAt(0);
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        System.out.println(m.solution(word, c));
+        int person = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
+
+        String danger = br.readLine();
+
+        System.out.println(m.solution(person, target, danger));
     }
 }
