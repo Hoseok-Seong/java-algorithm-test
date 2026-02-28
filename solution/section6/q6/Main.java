3cf0503bdb9e14d0c239e8ea794028228a656f78
hoseokseong <hoselabs@gmail.com>
2026-02-28 20:53:37 +0900

    6. 공주 구하기

--- solution/section6/q6/Main.java
+++ solution/section6/q6/Main.java
@@ -1,5 +1,43 @@
 package section6.q6;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    
+    public int solution(int prince, int number) {
+        int answer = 0;
+
+        Queue<Integer> circle = new ArrayDeque();
+
+        for (int i=1; i<=prince; i++) {
+            circle.offer(i);
+        }
+
+        while (!circle.isEmpty()) {
+            for (int i=1; i<number; i++) {
+                circle.offer(circle.poll());
+            }
+
+            circle.poll();
+
+            if (circle.size() == 1) {
+                answer = circle.poll();
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Main m = new Main();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int prince = Integer.parseInt(st.nextToken());
+        int number = Integer.parseInt(st.nextToken());
+
+        System.out.println(m.solution(prince, number));
+    }
 }
