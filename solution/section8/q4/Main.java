0ef91e2ac715e4084ecb7f2f8e2d7b4f4b14a2aa
hoseokseong <hoselabs@gmail.com>
2026-03-01 21:49:15 +0900

    4. 피보나치 재귀(메모이제이션)

--- solution/section8/q4/Main.java
+++ solution/section8/q4/Main.java
@@ -1,22 +1,90 @@
 package section8.q4;
 
-import java.io.*;
-import java.util.*;
+// 가장 기초 방법
+// public class Main {
+//     public int recursive(int n) {
+//         if (n == 1) {
+//             return 1;
+//         }
+//         else if (n == 2) {
+//             return 1;
+//         } 
+//         else {
+//             return recursive(n-1) + recursive(n-2);
+//         }
+//     }
 
+//     public static void main(String[] args) throws Exception {
+//         Main m = new Main();
+
+//         int n = 45;
+
+//         for (int i=1; i<n+1; i++) {
+//             System.out.print(m.recursive(i) + " ");
+//         }
+//     }
+// }
+
+// 배열에 저장하여 개선
+// public class Main {
+//     static int[] arr;
+
+//     public int recursive(int n) {
+//         if (n == 1) {
+//             return arr[n] = 1;
+//         }
+//         else if (n == 2) {
+//             return arr[n] = 1;
+//         } 
+//         else {
+//             return arr[n] = recursive(n-1) + recursive(n-2);
+//         }
+//     }
+
+//     public static void main(String[] args) throws Exception {
+//         Main m = new Main();
+
+//         int n = 45;
+
+//         arr = new int[n+1];
+
+//         m.recursive(n);
+
+//         for (int i=1; i<n+1; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+//     }
+// }
+
+// 메모이제이션
 public class Main {
-    public int solution(String word, char c) {
-        int answer = 0;
-        return answer;
+    static int[] arr;
+
+    public int recursive(int n) {
+        if (arr[n] > 0) return arr[n];
+
+        if (n == 1) {
+            return arr[n] = 1;
+        }
+        else if (n == 2) {
+            return arr[n] = 1;
+        } 
+        else {
+            return arr[n] = recursive(n-1) + recursive(n-2);
+        }
     }
 
     public static void main(String[] args) throws Exception {
         Main m = new Main();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = 45;
 
-        String word = br.readLine();
-        char c = br.readLine().charAt(0);
+        arr = new int[n+1];
 
-        System.out.println(m.solution(word, c));
+        m.recursive(n);
+
+        for (int i=1; i<n+1; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
