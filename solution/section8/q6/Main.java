d1db33286cf0a24417580e38eef1636e09bd174b
hoseokseong <hoselabs@gmail.com>
2026-03-02 00:34:45 +0900

    6. 부분집합 구하기(DFS)

--- solution/section8/q6/Main.java
+++ solution/section8/q6/Main.java
@@ -1,5 +1,44 @@
 package section8.q6;
 
+import java.util.*;
+
+// 3
+// 1 2 3
+// 1 2
+// 1 3
+// 1
+// 2 3
+// 2
+// 3
 public class Main {
-    
+    static int n;
+    static int[] arr;
+
+    public void dfs(int t) {
+        if (t == n+1) {
+            StringBuilder sb = new StringBuilder();
+            for (int i=1; i<=n; i++) {
+                if (arr[i] == 1) {
+                    sb.append(i + " ");
+                }
+            }
+            // 공집합 제외
+            if (sb.toString() != "") {
+                System.out.println(sb.toString());
+            }
+        } else {
+            arr[t] = 1;
+            dfs(t+1);
+            arr[t] = 0;
+            dfs(t+1);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Main m = new Main();
+
+        n = 3;
+        arr = new int[n+1];
+        m.dfs(1);
+    }
 }
