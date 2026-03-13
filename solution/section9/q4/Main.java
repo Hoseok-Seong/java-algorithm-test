1f2dd215482953ea23d23fe00aa834ca38629620
hoseokseong <hoselabs@gmail.com>
2026-03-13 23:20:15 +0900

    5. 동전교환

--- solution/section9/q4/Main.java
+++ solution/section9/q4/Main.java
@@ -1,5 +1,43 @@
 package section9.q4;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    
+    static int x;
+    static int y;
+    static int[] arr;
+
+    public void dfs(int level) {
+        if (level == y) {
+            for (int x : arr) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+        else {
+            for (int i=1; i<=x; i++) {
+                arr[level] = i;
+                dfs(level+1);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Main m = new Main();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 1부터 x까지
+        x = Integer.parseInt(st.nextToken());
+
+        // y번 뽑는다
+        y = Integer.parseInt(st.nextToken());
+
+        arr = new int[y];
+
+        m.dfs(0);
+    }
 }
--- solution/section9/q5/Main.java
+++ solution/section9/q5/Main.java
@@ -1,5 +1,54 @@
 package section9.q5;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    
+    static int x;
+    static int y;
+    static Integer[] arr;
+    static int answer = Integer.MAX_VALUE;
+
+    public void dfs(int level, int sum) {
+        if (level >= answer) {
+            return;
+        }
+
+        if (sum > y) {
+            return;
+        }
+
+        if (sum == y) {
+            answer = Math.min(answer, level);
+        }
+        else {
+            for (int i=0; i<x; i++) {
+                dfs(level+1, sum+arr[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Main m = new Main();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        x = Integer.parseInt(br.readLine());
+
+        arr = new Integer[x];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i=0; i<x; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        y = Integer.parseInt(br.readLine());
+
+        m.dfs(0, 0);
+
+        System.out.println(answer);
+    }
 }
